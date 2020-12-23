86d5d7c5901223671a8f0fb62c50ee50039fd3df
Lakshya Pandey <74096902+lakshyapandey1250@users.noreply.github.com>
2020-12-24 01:41:43 +0530

    Made The Code More Readable

--- Converter.py
+++ Converter.py
@@ -1,5 +1,6 @@
 # ----- Imports -----
 from openexchangerate import OpenExchangeRates
+from tkinter import messagebox
 from tkinter import *
 import subprocess as sp
 import json
@@ -38,57 +39,61 @@ def get_rates():
 
 def convert(input1, input2, amount):
     """Converting The Currencies"""
-    isfull1 = True
-    isfull2 = True
-    short1 = None
-    short2 = None
-    if len(input1) == 3:
-        input1 = input1.upper()
-        isfull1 = False
-        num1 = latest[input1]
-        short1 = input1
-    if len(input2) == 3:
-        input2 = input2.upper()
-        isfull2 = False
-        num2 = latest[input2]
-        short2 = input2
-    if isfull1:
-        if input1.count(" ") > 0:
-            input1 = input1.split(" ")
-            for item in input1:
-                item.capitalize()
-            input1 = " ".join(input1)
-            for key, value in names.items():
-                if value == input1:
-                    num1 = latest[key]
-                    short1 = key
-        else:
-            input1 = input1.capitalize()
-            for key, value in names.items():
-                if value == input1:
-                    num1 = latest[key]
-                    short1 = key
-    if isfull2:
-        if input2.count(" ") > 0:
-            input2 = input2.split(" ")
-            for item in input2:
-                item.capitalize()
-            input2 = " ".join(input2)
-            for key, value in names.items():
-                if value == input2:
-                    num2 = latest[key]
-                    short2 = key
-                    
-        else:
-            input2 = input2.capitalize()
-            for key, value in names.items():
-                if value == input2:
-                    num2 = latest[key]
-                    short2 = key
-    am = num2 / num1
-    final = amount * am
-    final_text = str(str(amount)+" "+names[short1]+" Are \n"+str(final)+" "+names[short2]+" ")
-    label2["text"] = final_text
+    if input1 == "" or input2 == "" or amount == "":
+        messagebox.showerror("Empty Fields", "One Or More Fields Are Empty. Please Fill All Fields.")
+    if int(amount) <= 0:
+        messagebox.showerror("Wrong Input", "You Have Entered Negative Input. Please Enter Positive Input.")
+    else:
+        isfull1 = True
+        isfull2 = True
+        short1 = None
+        short2 = None
+        if len(input1) == 3:
+            input1 = input1.upper()
+            isfull1 = False
+            num1 = latest[input1]
+            short1 = input1
+        if len(input2) == 3:
+            input2 = input2.upper()
+            isfull2 = False
+            num2 = latest[input2]
+            short2 = input2
+        if isfull1:
+            if input1.count(" ") > 0:
+                input1 = input1.split(" ")
+                for item in input1:
+                    item.capitalize()
+                input1 = " ".join(input1)
+                for key, value in names.items():
+                    if value == input1:
+                        num1 = latest[key]
+                        short1 = key
+            else:
+                input1 = input1.capitalize()
+                for key, value in names.items():
+                    if value == input1:
+                        num1 = latest[key]
+                        short1 = key
+        if isfull2:
+            if input2.count(" ") > 0:
+                input2 = input2.split(" ")
+                for item in input2:
+                    item.capitalize()
+                input2 = " ".join(input2)
+                for key, value in names.items():
+                    if value == input2:
+                        num2 = latest[key]
+                        short2 = key                    
+            else:
+                input2 = input2.capitalize()
+                for key, value in names.items():
+                    if value == input2:
+                        num2 = latest[key]
+                        short2 = key
+        am = num2 / num1
+        final = int(amount) * am
+        final_text = str(str(amount)+" "+names[short1]+" Are \n"+str(final)+" "+names[short2]+" ")
+        label2["text"] = final_text
     
 # ----- Main Code -----
 
@@ -106,41 +111,41 @@ background_label.place(relheight=1, relwidth=1)
 root.iconbitmap("Icon.ico")
 
 # Creating The Main Window
-frame1 = Frame(root,bg="white")
-frame1.place(relx=0.1,rely=0.1,relwidth=0.8,relheight=0.8)
+frame1 = Frame(root, bg="white")
+frame1.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
 
 label = Label(frame1, text="Currency Converter", font=("Times New Roman", 20), fg="Black", bg="White")
 label.place(relwidth=0.5, relheight=0.08, relx=0.26, rely=0.055)
 
-info1 = Button(frame1, relief=RIDGE, text="i", font=("Times New Roman", 20), fg="Black", bg="White", command=helper, cursor="hand2")
+info1 = Button(frame1, relief=RIDGE, text="i", font=("Times New Roman", 20), fg="Black", bg="White", bd=5, command=helper, cursor="hand2")
 info1.place(relwidth=0.1, relheight=0.08, relx=0.85, rely=0.05)
 
-froml = Label(frame1, text="From:",font=("Times New Roman",20),fg="Black",bg="White")
-froml.place(relx=0.1,rely=0.2,relwidth=0.2,relheight=0.1)
+froml = Label(frame1, text="From:", font=("Times New Roman", 20), fg="Black", bg="White")
+froml.place(relx=0.1, rely=0.2, relwidth=0.2, relheight=0.1)
 
-entry1 = Entry(frame1,relief=GROOVE,bg="White",bd=7,font=("Times New Roman",20),fg="Black")
-entry1.place(relx=0.3,rely=0.2,relwidth=0.6,relheight=0.1)
+entry1 = Entry(frame1, relief=RIDGE, bg="White", bd=7, font=("Times New Roman", 20), fg="Black")
+entry1.place(relx=0.3, rely=0.2, relwidth=0.6, relheight=0.1)
 
-from2 = Label(frame1,text="To:",font=("Times New Roman",20),fg="Black",bg="White")
-from2.place(relx=0.1,rely=0.375,relwidth=0.2,relheight=0.1)
+from2 = Label(frame1, text="To:", font=("Times New Roman", 20), fg="Black", bg="White")
+from2.place(relx=0.1, rely=0.375, relwidth=0.2, relheight=0.1)
 
-entry2 = Entry(frame1,relief=GROOVE,bg="White",bd=7,font=("Times New Roman",20),fg="Black")
-entry2.place(relx=0.3,rely=0.375,relwidth=0.6,relheight=0.1)
+entry2 = Entry(frame1, relief=RIDGE, bg="White", bd=7, font=("Times New Roman", 20), fg="Black")
+entry2.place(relx=0.3, rely=0.375, relwidth=0.6, relheight=0.1)
 
-from3 = Label(frame1,text="Amount:",font=("Times New Roman",20),fg="Black",bg="White")
-from3.place(relx=0.08,rely=0.55,relwidth=0.2,relheight=0.1)
+from3 = Label(frame1, text="Amount:", font=("Times New Roman", 20), fg="Black", bg="White")
+from3.place(relx=0.08, rely=0.55, relwidth=0.2, relheight=0.1)
 
-entry3 = Entry(frame1,relief=GROOVE,bg="White",bd=7,font=("Times New Roman",20),fg="Black")
-entry3.place(relx=0.3,rely=0.55,relwidth=0.6,relheight=0.1)
+entry3 = Entry(frame1, relief=RIDGE, bg="White", bd=7, font=("Times New Roman", 20), fg="Black")
+entry3.place(relx=0.3, rely=0.55, relwidth=0.6, relheight=0.1)
 
-button1 = Button(frame1,relief=RIDGE,text="Convert",bd=7,bg="White",fg="Black",command=lambda:convert(entry1.get(), entry2.get(), entry3.get()), font=("Times New Roman", 15), cursor="hand2")
-button1.place(relx=0.1,rely=0.7,relwidth=0.35,relheight=0.1)
+button1 = Button(frame1, relief=RIDGE, text="Convert", font=("Times New Roman", 20), bd=7, bg="White", fg="Black", command=lambda:convert(entry1.get(), entry2.get(), entry3.get()), cursor="hand2")
+button1.place(relx=0.1, rely=0.7, relwidth=0.35, relheight=0.1)
 
-button2 = Button(frame1,relief=RIDGE,text="View Codes",bd=7,bg="White",fg="Black",command=lambda:sp.Popen(["notepad.exe", "Codes.txt"]), font=("Times New Roman", 15), cursor="hand2")
-button2.place(relx=0.55,rely=0.7,relwidth=0.35,relheight=0.1)
+button2 = Button(frame1, relief=RIDGE, text="View Codes", font=("Times New Roman", 20), bd=7, bg="White", fg="Black", command=lambda:sp.Popen(["notepad.exe", "Codes.txt"]), cursor="hand2")
+button2.place(relx=0.55, rely=0.7, relwidth=0.35, relheight=0.1)
 
-label2 = Label(frame1,font=("Times New Roman", 15), fg="Black", bg="white")
-label2.place(relx=0.1, rely=0.8, relwidth=0.8, relheight=0.2)    
+label2 = Label(frame1, font=("Times New Roman", 20), fg="Black", bg="white")
+label2.place(relx=0.05, rely=0.8, relwidth=0.9, relheight=0.2)    
 
 # ----- Driver Code -----
 if __name__ == "__main__":
